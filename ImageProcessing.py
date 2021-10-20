6cd166b949a1b364e356e5dec6820beda6a8e3a4
pstilian <pstilian111@gmail.com>
2021-10-20 14:50:07 -0400

    updated image processing file

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -6,6 +6,7 @@ Created on Mon Oct 11 14:24:39 2021
 """
 
 import cv2
+import os
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -15,8 +16,8 @@ of input images an output an array of grayscale images
 Parmeters: image, The filepath for the image to be downloaded  Returns: gray_image a grayscale version of original image
 """
 def Grayscale(image):
-    img = cv2.imread(image, 0)
-    #cv2.imshow('Original Image', im)
+    img = cv2.imread(image, cv2.IMREAD_UNCHANGED)
+    cv2.imshow('Original Image', img)
     #cv2.waitKey(0)
     
     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -25,27 +26,43 @@ def Grayscale(image):
     #cv2.waitKey(0)
     
     #cv2.destroyAllWIndows()
+    
+    #cv2.imwrite('E:\gray.png',gray_image)
 
     return gray_image    
 
-"""
-"""
+
+
 def Canny_Edge(image):
     img = cv2.imread(image, 0)
-    edges = cv2.Canny(img, 100, 200)  # 2nd and 3rd parameters are lower and higer intensity gradients
-    
+    edges = cv2.Canny(img, 80, 110)  # 2nd and 3rd parameters are lower and higer intensity gradients
+    edges2 = cv2.Canny(img, 30, 150)
     #cv2.imshow('Canny Image', edges)
-    #cv2.waitkey(0)
+    #cv2.waitKey(0)
     
-    return edges
+    #cv2.imwrite('E:\canny.png',edges)
+    
+    return edges, edges2
 
-"""
-"""
+
+
 def Gauss_Blur(image):
-    img = cv2.imread(image, 0)
-    blur = cv2.GaussianBlur(img, (5, 5), 0) # 2nd parameter is responsible for blurring kernel greater size = greater blur
+    img = cv2.imread(image, cv2.IMREAD_UNCHANGED)
+    blur = cv2.GaussianBlur(img, (55, 55), 0) # 2nd parameter is responsible for blurring kernel greater size = greater blur
     
     #cv2.imshow('Blurred Image', blur)
     #cv2.waitKey(0)
     
+    #cv2.imwrite('E:\blur.png',blur)
+    
     return blur
+
+g = Grayscale('E:\Project 1 Database\AntonioLaverghetta/BL_BL_2.png')
+cv2.imwrite('E:\gray.png',g)
+    
+c1, c2 = Canny_Edge('E:\Project 1 Database\AntonioLaverghetta/BL_BL_2.png')
+cv2.imwrite('E:\canny.png',c1)
+cv2.imwrite('E:\canny2.png',c2)
+
+ga = Gauss_Blur('E:\Project 1 Database\AntonioLaverghetta/BL_BL_2.png')
+cv2.imwrite('E:\gauss.png',ga)
